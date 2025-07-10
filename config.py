7bf659370ced8c8abc95e1fc2b588b38148501f1
VootXMusic <updatebot59@gmail.com>
2025-07-10 18:14:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bairagi2oo3/ShizukaMusicBot4",
+    "https://github.com/VOOTTV143/BotXBazaar",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
