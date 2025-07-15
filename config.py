3ca18f711e7b3a58af148a4c34d77005bc2b5537
VootXMusic <updatebot59@gmail.com>
2025-07-15 06:30:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AboutBotMaker")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/KING_BOT_UPDATE")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BotXBazaar")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BotXBazaar")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
